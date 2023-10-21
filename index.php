bb9a9cbcaa378647aab5c26e9521185317017712
Ivan Elizalde <ielizalde185@gmail.com>
2023-10-20 23:23:04 -0600

    Se muestran correctamente los archivos dentro del directorio ejemploPDF

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webdav</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
 
-    <link rel="stylesheet" href="VistaUsuario/css/navbar.css">
-    <link rel="stylesheet" href="VistaUsuario/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
@@ -16,11 +18,11 @@
         integrity="sha512-KXmDlF1ri1m3+oQTxo2dPXHoqr/qndCS0fVwX6rD38eq8yzG0BZNA26DrpYToPENdzmsS2IAGU8AMr8NZlTjMQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<body>
+<body style="background-color: white;">
     <!--NAVBAR-->
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-moradito">
         <div class="container-fluid">
-            <img src="img/icons/KSPGames.png" class="navbar-brand img-fluid" alt="Logo">
+            <img src="assets/KSPGames.png" class="navbar-brand img-fluid" alt="Logo">
             <!-- <a href="#" class="navbar-brand">KSPGAMES</a> -->
             <button class="navbar-toggler m-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
                 aria-controls="menu" aria-expanded="false" aria-label="Mostrar / Ocultar menu">
@@ -31,62 +33,73 @@
                     <li class="nav-item"><a href="#" class="nav-link disabled">WebDav</a></li>
                 </ul>
                 <div>
-                    <a href="http://10.0.33.2/DesarrolloWebProyecto/VistaUsuario/html/indexUser.php?user=ielizalde185@gmail.com" class="btn btn-outline-secondary">Regresar</a>
+                    <a href="VistaUsuario/html/login.html" class="btn btn-outline-secondary me-1">usuario</a>
+                    <a href="VistaUsuario/html/registro.html" class="btn btn-outline-primary">Regresar</a>
                 </div>
             </div>
         </div>
     </nav>
-    
+
     <main class="container">
-        <div class="row">
+        <div class="row my-5 py-5">
             <div class="col-12">
-                <h2>Listado de pedidos</h2>
+                <h2>Historial de pedidos de pedidos</h2>
             </div>
-        </div>
-        <ul>
-            <?php 
-            $directorio = 'ejemploPDF';
+            
+            <!-- <div class="col-12">
+                <div class="row">
+                    <div class="col-1 d-flex align-items-center">
+                        <i class=" h2 bi bi-file-pdf-fill"></i>
+                        <a href="#">nombreArchivo.pdf</a>
+                    </div>
+                </div>
+            </div> -->
+
+            <?php
+            $directorio = 'ejemploPDF/';
             $archivos = scandir($directorio);
 
-            foreach ($archivos as $archivo){
-                if ($archivo != '.'&& $archivo != '..'){
-                    echo '<li><a href="'.$directorio.'/'.$archivo.'">'.$archivo.'</a></li>';
+            foreach ($archivos as $archivo) {
+                if ($archivo != '.' && $archivo != '..') {
+                    ?>
+                    <div class="col-12">
+                        <div class="row">
+                            <div class="col-12 d-flex align-items-center">
+                                <i class=" h2 bi bi-file-pdf-fill"></i>
+                                <a href="<?php echo $directorio . '/' . $archivo ?>">
+                                    <?php echo $archivo ?>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
                 }
             }
             ?>
-        </ul>
-
-        <div class="row">
-            <div class="col-12">
-                <table class="table-responsive table-striped">
-                    <thead>
-                        <tr class="table-dark">
-                            <th>id</th>
-                            <th>Archivo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Pedido.pdf</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-            </div>
+            <?php
+            // $directorio = 'ejemploPDF';
+            // $archivos = scandir($directorio);
+            
+            // foreach ($archivos as $archivo) {
+            //     if ($archivo != '.' && $archivo != '..') {
+            //         echo '<li><a href="' . $directorio . '/' . $archivo . '">' . $archivo . '</a></li>';
+            //     }
+            // }
+            ?>
         </div>
+
     </main>
 
     <!--FOOTER-->
-    <footer style="background: black;">
+    <footer style="background: black;" class="">
         <div class="container text-white">
             <div class="row pt-5">
                 <div class="col-12 col-md-4 mb-4">
                     <h2>Encuentranos en:</h2>
-                    <a href="#"><img src="img/icons/discordIcon.png" alt="" class="img-fluid"></a>
-                    <a href="#"><img src="img/icons/facebookIcon.png" alt="" class="img-fluid"></a>
-                    <a href="#"><img src="img/icons/instaIcon.png" alt="" class="img-fluid"></a>
-                    <a href="#"><img src="img/icons/twitterIcon.png" alt="" class="img-fluid"></a>
+                    <a href="#"><i class="h1 bi bi-facebook"></i></a>
+                    <a href="#"><i class="h1 bi bi-discord "></i></a>
+                    <a href="#"><i class="h1 bi bi-instagram"></i></a>
+                    <a href="#"><i class="h1 bi bi-twitter"></i></a>
                 </div>
 
                 <div class="col-12 col-md-4 mb-4">
@@ -116,4 +129,5 @@
 
     <script src="js/bootstrap.bundle.js"></script>
 </body>
+
 </html>
