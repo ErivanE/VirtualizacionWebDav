f765f96179049939485fe86ef814d8111965d720
ErivanE <ielizalde185@gmail.com>
2023-10-21 12:43:34 -0600

    mostrar pdf

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
             </div> -->
 
             <?php
-            $directorio = 'ejemploPDF/';
+            $directorio = '/home/pdf/';
             $archivos = scandir($directorio);
 
             foreach ($archivos as $archivo) {
